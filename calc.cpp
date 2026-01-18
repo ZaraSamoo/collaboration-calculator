8cb299d6099e5c07e30a1a55e23edcf8ea86f26b
ZaraSamoo <141186185+ZaraSamoo@users.noreply.github.com>
2026-01-18 21:39:33 +0500

    merge resolved

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int add(int x, int y) {
+    return x + y;
+}
+
+int sub(int x, int y) {
+    return x - y;
+}
+
+
 int main() {
     cout << "Calculator Application" << endl;
     return 0;
